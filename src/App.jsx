eb7ee9f74f366bcd8761c0491ac3d808f3e0a5d3
Muhammad Raza <103913857+mohammadraza123@users.noreply.github.com>
2022-11-25 22:55:38 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,10 +14,13 @@ function App() {
        <h1 className="heading">OLX CARD</h1>
       <div className='second'>
 
-       <Card title="Phone"/>
-       <Card />
-       <Card/>
-       <Card/>
+       <Card imgsrc= "olx1.jpeg"  title= "Mehran Car" rs= "Rs 5,70000" address= "Port Qasim, Karachi . 5 days ago" featured=  {true}/>
+            <Card imgsrc= "olx3.jpeg" title= "Vivo Y15s" rs= "Rs 60,000" address= "Shalimar ,Lahore . 5 days ago" featured=  {true}/>
+            <Card imgsrc= "olx6.jpeg" title= "LENOVO THINKPAD" rs= "Rs 40,000" address= "Dhok Kala Khan,Quetta . 5 days ago" featured=  {true}/>
+            <Card imgsrc= "olx4.jpeg" title= "Realme Mobile" rs= "Rs 50,000" address= "Chaman, Balochistan . 5 week ago" featured=  {false}/>
+            <Card imgsrc= "olx5.jpeg" title= "Electric" rs= "Rs 5,70000" address= "New Town, Hyderabad . 1 month ago" featured=  {true}/>
+            <Card imgsrc= "olx7.jpeg" title= "Betlis Sofa" rs= "Rs 30000" address= "Malir, Karachi . 9 days ago" featured=  {false}/>
+            <Card imgsrc= "olx2.jpeg" title= "Furniture Shop" rs= "Rs 1,70000" address= "Saddar, Karachi . 2 week ago" featured=  {false}/>
     
       </div>
     </>
